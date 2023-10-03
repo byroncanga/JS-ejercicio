135339d3dd6fb3a436243920b9ea631831193515
Byron Canga <65374818+byroncanga@users.noreply.github.com>
2023-10-03 14:44:23 +0000

    commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,28 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  document.querySelector("#btnGen").addEventListener("click", () => {
+    document.querySelector("#escusa").innerHTML = escusasGen();
+  });
+};
+let escusasGen = () => {
+  let pronombre = ["un", "el"];
+  let sujeto = ["perro", "gato", "caballo", "conductor", "mi hijo"];
+  let accion = ["se me atravezo", "me asusto", "se lastimó"];
+  let donde = ["en mi casa", "en la calle", "en la vía"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronomIndex = Math.floor(Math.random() * pronombre.length);
+  let sujetoIndex = Math.floor(Math.random() * sujeto.length);
+  let accionIndex = Math.floor(Math.random() * accion.length);
+  let dondeIndex = Math.floor(Math.random() * donde.length);
+
+  return (
+    pronombre[pronomIndex] +
+    " " +
+    sujeto[sujetoIndex] +
+    " " +
+    accion[accionIndex] +
+    " " +
+    donde[dondeIndex]
+  );
 };
